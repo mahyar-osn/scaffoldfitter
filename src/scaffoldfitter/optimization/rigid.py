c2947e791972a2ec16eecb453833a74b7ec5a12e
mahyar-osn <m.osanlouy@auckland.ac.nz>
2019-05-23 13:45:15 +1200

    A brief doc on the class.

--- src/scaffoldfitter/optimization/rigid.py
+++ src/scaffoldfitter/optimization/rigid.py
@@ -1,6 +1,15 @@
 
 
-class Rigid1(object):
+class Rigid(object):
+    """
+    Rigid object class to handle rigid computations and optimization.
+    Here a probabilistic model is used i.e. the alignment of the scaffold & data cloud is a probability density estimation
+    problem. Initially, a Gaussian Mixture Model (GMM) centroid from the scaffold is fitted to the point cloud by maximizing the
+    likelihood. A "coherence" constraint is imposed by reparameterization of GMM centroids with rigid parameters (i.e. scaling,
+    translation, and rotation) and the closed form solution of the maximization step of the Expectation-Maximization algorithm
+    is derived.
+    """
+
     def __init__(self):
         pass
 
